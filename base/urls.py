fa174dbbc66ab30b0157c0464b22837c408d3a30
Mehfooz Ali <61369686+Mehfooz201@users.noreply.github.com>
2023-10-11 01:09:59 +0500

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -13,7 +13,10 @@ urlpatterns = [
     path('deduct-credits/', views.deduct_credits, name='deduct_credits'),
 
     path('enroll-course/<str:slug>/', views.EnrollCource, name='enroll'),
+    
     path('courses/', views.courses, name='courses'),
+
+    path('donate-now/', views.donateNow, name='donate'),
 
  
 ]
